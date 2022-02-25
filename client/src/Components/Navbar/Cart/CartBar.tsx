b8177338a436dced549b9734f9c6639764a46ca6
Vito Medlej <rip.vitto@gmail.com>
2022-02-25 19:08:37 +0200

    Update CartBar.tsx

--- client/src/Components/Navbar/Cart/CartBar.tsx
+++ client/src/Components/Navbar/Cart/CartBar.tsx
@@ -1,3 +1,34 @@
+import React from 'react'
+import CloseIcon from '@mui/icons-material/Close';
+
+import CBox from '../../CustomMui/CBox';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+
+const CartBar = () => {
+    return (
+        <CBox
+            sx={{
+            width: '100%',
+            background: 'white',
+            top: '100%',
+            zIndex: '55',
+            display: {
+                xs: 'flex',
+                md: 'none'
+            },
+            height: '300px',
+            borderTop: '1px solid #8080802b',
+            color: 'black',
+            position: 'absolute'
+        }}>
+            <Box
+                className='flexed'
+                sx={{
+                width: '93.5%',
+                height: '50px',
+                display: 'flex',
+                justifyContent: 'flex-end'
             }}>
                 <IconButton
                     sx={{
